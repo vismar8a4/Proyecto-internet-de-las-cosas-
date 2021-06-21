d742d2285e89df6a9d7a82712cbbd52a3e417346
vismar8a4 <vismar8a4@gmail.com>
2021-06-21 16:17:53 -0400

    proyecto ico

---
+++ sistema de puertas/WindowsApp2/WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,349 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(25, 133)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(585, 495)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(121, 36)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(149, 47)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "CERRAR/TODO"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(426, 36)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(149, 47)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "ABRIR/TODO"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(647, 118)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(135, 43)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "PUERTA/TRASERA"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(647, 201)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(135, 39)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "PUERTA/PRINCIPAL"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(647, 279)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(135, 40)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "VENTANA/NORTE 1"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(647, 355)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(135, 36)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "VENTANA/NORTE 2"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(647, 435)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(135, 39)
+        Me.Button7.TabIndex = 7
+        Me.Button7.Text = "PUERTA/PRINCIPAL 2"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(647, 520)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(135, 45)
+        Me.Button8.TabIndex = 8
+        Me.Button8.Text = "PUERTA/PROYECTOR"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(647, 605)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(135, 39)
+        Me.Button9.TabIndex = 9
+        Me.Button9.Text = "VENTANA/SUR"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(840, 133)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(840, 212)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(840, 290)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(840, 366)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(840, 446)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(840, 520)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Label6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(840, 616)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Label7"
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM3"
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(25, 634)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(135, 39)
+        Me.Button10.TabIndex = 17
+        Me.Button10.Text = "CONECTAR"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(475, 634)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(135, 39)
+        Me.Button11.TabIndex = 18
+        Me.Button11.Text = "DESCONECTAR"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(101, 187)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox1.TabIndex = 19
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(284, 187)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(49, 20)
+        Me.TextBox2.TabIndex = 20
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(203, 561)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(49, 20)
+        Me.TextBox3.TabIndex = 21
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(509, 561)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(49, 20)
+        Me.TextBox4.TabIndex = 22
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(539, 397)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(49, 20)
+        Me.TextBox5.TabIndex = 23
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(498, 487)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(42, 20)
+        Me.TextBox6.TabIndex = 24
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(436, 187)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(47, 20)
+        Me.TextBox7.TabIndex = 25
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(956, 674)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+End Class
---
+++ sistema de puertas/WindowsApp2/WindowsApp2/Form1.vb
@@ -0,0 +1,231 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Threading
+Public Class Form1
+    Private Sub Form1_Disposed(ByVal sender As Object, ByVal e As System.EventArgs)
+        SerialPort1.Close()
+    End Sub
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+    Shared _continue As Boolean
+    Shared _serialPort As SerialPort
+    Dim cambio12 As Boolean = True
+    Dim cambio13 As Boolean = True
+    Dim cambio14 As Boolean = True
+    Dim cambio15 As Boolean = True
+    Dim cambio16 As Boolean = True
+    Dim cambio17 As Boolean = True
+    Dim cambio18 As Boolean = True
+    Dim cambio19 As Boolean = True
+    Dim cambio20 As Boolean = True
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SerialPort1.Close()
+        SerialPort1.Open()
+        If cambio19 = True Then
+            Button1.BackColor = Color.OrangeRed
+
+            SerialPort1.Write("m")
+            Label1.Text = "CERRADO"
+            Label2.Text = "CERRADO"
+            Label3.Text = "CERRADO"
+            Label4.Text = "CERRADO"
+            Label5.Text = "CERRADO"
+            Label6.Text = "CERRADO"
+            Label7.Text = "CERRADO"
+            TextBox1.BackColor = Color.Red
+            TextBox2.BackColor = Color.Red
+            TextBox3.BackColor = Color.Red
+            TextBox4.BackColor = Color.Red
+            TextBox5.BackColor = Color.Red
+            TextBox6.BackColor = Color.Red
+            TextBox7.BackColor = Color.Red
+
+
+        Else
+            Button1.BackColor = Color.WhiteSmoke
+
+        End If
+        cambio19 = Not (cambio19)
+        SerialPort1.Close()
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        SerialPort1.Close()
+        SerialPort1.Open()
+        Button10.Enabled = False
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        SerialPort1.Close()
+        SerialPort1.Open()
+        If cambio13 = True Then
+            Button3.BackColor = Color.GreenYellow
+            SerialPort1.Write("a")
+            Label1.Text = "ABIERTO"
+            TextBox1.BackColor = Color.GreenYellow
+        Else
+            Button3.BackColor = Color.OrangeRed
+            SerialPort1.Write("e")
+            Label1.Text = "CERRADO"
+            TextBox1.BackColor = Color.OrangeRed
+
+        End If
+        cambio13 = Not (cambio13)
+        SerialPort1.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SerialPort1.Close()
+        SerialPort1.PortName = "COM5" 'CAMBIA EL PUERTO COM
+        SerialPort1.BaudRate = 9600
+        SerialPort1.DataBits = 8
+        SerialPort1.Parity = Parity.None
+        SerialPort1.StopBits = StopBits.One
+        SerialPort1.Handshake = Handshake.None
+        SerialPort1.Encoding = System.Text.Encoding.Default
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        SerialPort1.Close()
+        SerialPort1.Open()
+        If cambio12 = True Then
+            Button4.BackColor = Color.GreenYellow
+            SerialPort1.Write("i")
+            Label2.Text = "ABIERTO"
+            TextBox5.BackColor = Color.GreenYellow
+
+        Else
+            Button4.BackColor = Color.OrangeRed
+            SerialPort1.Write("o")
+            Label2.Text = "CERRADO"
+            TextBox5.BackColor = Color.OrangeRed
+
+        End If
+        cambio12 = Not (cambio12)
+        SerialPort1.Close()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        SerialPort1.Close()
+        SerialPort1.Open()
+        If cambio14 = True Then
+            Button5.BackColor = Color.GreenYellow
+            SerialPort1.Write("u")
+            Label3.Text = "ABIERTO"
+            TextBox2.BackColor = Color.GreenYellow
+        Else
+            Button5.BackColor = Color.OrangeRed
+            SerialPort1.Write("b")
+            Label3.Text = "CERRADO"
+            TextBox2.BackColor = Color.OrangeRed
+        End If
+        cambio14 = Not (cambio14)
+        SerialPort1.Close()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        SerialPort1.Close()
+        SerialPort1.Open()
+        If cambio15 = True Then
+            Button6.BackColor = Color.GreenYellow
+            SerialPort1.Write("c")
+            Label4.Text = "ABIERTO"
+            TextBox7.BackColor = Color.GreenYellow
+        Else
+            Button6.BackColor = Color.OrangeRed
+            SerialPort1.Write("d")
+            Label4.Text = "CERRADO"
+            TextBox7.BackColor = Color.OrangeRed
+
+        End If
+        cambio15 = Not (cambio15)
+        SerialPort1.Close()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        SerialPort1.Close()
+        SerialPort1.Open()
+        If cambio16 = True Then
+            Button7.BackColor = Color.GreenYellow
+            SerialPort1.Write("g")
+            Label5.Text = "ABIERTO"
+            TextBox4.BackColor = Color.GreenYellow
+        Else
+            Button7.BackColor = Color.OrangeRed
+            SerialPort1.Write("f")
+            Label5.Text = "CERRADO"
+            TextBox4.BackColor = Color.OrangeRed
+
+        End If
+        cambio16 = Not (cambio16)
+        SerialPort1.Close()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        SerialPort1.Close()
+        SerialPort1.Open()
+        If cambio17 = True Then
+            Button8.BackColor = Color.GreenYellow
+            SerialPort1.Write("h")
+            Label6.Text = "ABIERTO"
+            TextBox6.BackColor = Color.GreenYellow
+        Else
+            Button8.BackColor = Color.OrangeRed
+            SerialPort1.Write("j")
+            Label6.Text = "CERRADO"
+            TextBox6.BackColor = Color.OrangeRed
+
+        End If
+        cambio17 = Not (cambio17)
+        SerialPort1.Close()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        SerialPort1.Close()
+        SerialPort1.Open()
+        If cambio18 = True Then
+            Button9.BackColor = Color.GreenYellow
+            SerialPort1.Write("k")
+            Label7.Text = "ABIERTO"
+            TextBox3.BackColor = Color.GreenYellow
+        Else
+            Button9.BackColor = Color.OrangeRed
+            SerialPort1.Write("l")
+            Label7.Text = "CERRADO"
+            TextBox3.BackColor = Color.OrangeRed
+
+        End If
+        cambio18 = Not (cambio18)
+        SerialPort1.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SerialPort1.Close()
+        SerialPort1.Open()
+        If cambio20 = True Then
+            Button2.BackColor = Color.YellowGreen
+            SerialPort1.Write("n")
+            Label1.Text = "ABIERTO"
+            Label2.Text = "ABIERTO"
+            Label3.Text = "ABIERTO"
+            Label4.Text = "ABIERTO"
+            Label5.Text = "ABIERTO"
+            Label6.Text = "ABIERTO"
+            Label7.Text = "ABIERTO"
+            TextBox1.BackColor = Color.GreenYellow
+            TextBox2.BackColor = Color.GreenYellow
+            TextBox3.BackColor = Color.GreenYellow
+            TextBox4.BackColor = Color.GreenYellow
+            TextBox5.BackColor = Color.GreenYellow
+            TextBox6.BackColor = Color.GreenYellow
+            TextBox7.BackColor = Color.GreenYellow
+
+        Else
+            Button2.BackColor = Color.WhiteSmoke
+
+        End If
+        cambio20 = Not (cambio20)
+        SerialPort1.Close()
+    End Sub
+End Class
